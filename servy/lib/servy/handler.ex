7cbbe18fac4ae218e36fac9c85cbb054ab37eb02
Javier J. Martinez F <entrenamientospuntonet@gmail.com>
2026-06-24 11:19:49 -0400

    feat: Add pipeline and placeholder transformation functions

--- servy/lib/servy/handler.ex
+++ servy/lib/servy/handler.ex
@@ -1,5 +1,30 @@
 defmodule Servy.Handler do
   def handle(request) do
+    request
+    |> parse
+    |> route
+    |> format_response
+  end
+
+  def parse(request) do
+    # TODO: Parse the request string into a map:
+    conv = %{method: "GET", path: "/wildthings", resp_body: ""}
+  end
+
+  def parse(conv) do
+    # TODO: Create a new map that also has the response body:
+    conv = %{method: "GET", path: "/wildthings", resp_body: "Bears, Lions, Tigers"}
+  end
+
+  def format_response(conv) do
+    # TODO: Use values in the map to create an HTTP response string
+    """
+    HTTP/1.1 200 OK
+    Content-Type: text/html
+    Content-Length: 20
+
+    Bears, Lions, Tigers
+    """
   end
 end
 
@@ -9,14 +34,6 @@ Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
 
-"""
-
-expected_response = """
-HTTP/1.1 200 OK
-Content-Type: text/html
-Content-Length: 20
-
-Bears, Lions, Tigers
 """
 
 response = Servy.Handler.handle(request)
